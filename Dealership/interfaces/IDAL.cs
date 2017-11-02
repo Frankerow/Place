d28ff1da20f704e7bc1c1450fbcbf2ae72cf0bd9
Frankerow <frankerow@mail.ru>
2017-11-03 00:33:08 +0400

    чуть доработал интерфейс

--- Dealership/interfaces/IDAL.cs
+++ Dealership/interfaces/IDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,22 @@ namespace interfaces
 {
     public interface IDAL 
     {
-        bool AddProduct(Guid id,string country,string mark,string model,string presence,double price);
+        bool AddProduct(Guid id,string country,string mark,string model,string presence,double price, string bodytype, int numberofdoors, int numberofplaces, string euginetype, string euginelocation, string euginevolume);
         bool DeleteProduct();
         bool UpdateProduct();
+        Product GetOneProduct(Guid id, string country, string mark, string model, string presence, double price);
+        List <Product> GetAllProduct();
+
+        bool AddCustomer(Guid id, string thename, string adress, int phone, int serius, int number);
+        bool DeleteCustomer();
+        bool UpdateCustomer();
+        List<Customers> GetAllCustomers();
+
+
+
+
+
+
+
     }
 }
