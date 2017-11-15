9e753ff9dfa31d8d93fc5e19205d3b21e8d55826
Frankerow <frankerow@mail.ru>
2017-11-15 15:08:23 +0400

    15/11/17(1)

---
+++ Dealership/Dealership/Models/ProductVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using entities;
+
+namespace Dealership.Models
+{
+    public class ProductVM
+    {
+
+        public Guid Id { get; set; }
+        public string Country { get; set; }
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public string Presense { get; set; }
+        public double Price { get; set; }
+
+        public static bool AddProduct(ProductVM product,TechnicalDataVM technicalDataVM)
+        {
+            var listprod =new Product();
+            listprod.Id = product.Id;
+            listprod.Country = product.Country;
+            listprod.Mark = product.Mark;
+            listprod.Model = product.Model;
+            listprod.Presense = product.Presense;
+            listprod.Price = product.Price;
+
+            var listte = new TechnicalData();
+            listte.Id = technicalDataVM.Id;
+            listte.Numbersofdoors = technicalDataVM.Numbersofdoors;
+            listte.Numbersofplaces = technicalDataVM.Numbersofplaces;
+            listte.EugineType = technicalDataVM.EugineType;
+            listte.EugineLocation = technicalDataVM.EugineLocation;
+            listte.EugineVolume = technicalDataVM.EugineVolume;
+            listte.BodyType = technicalDataVM.BodyType;
+
+           return Repo.repo.AddProduct(listprod,listte);
+        }
+    }
+}
---
+++ Dealership/Dealership/Models/TechnicalDataVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dealership.Models
+{
+    public class TechnicalDataVM
+    {
+
+        public Guid Id { get; set; }
+        public string BodyType { get; set; }
+        public int Numbersofdoors { get; set; }
+        public int Numbersofplaces { get; set; }
+        public string EugineType { get; set; }
+        public string EugineLocation { get; set; }
+        public string EugineVolume { get; set; }
+    }
+}
